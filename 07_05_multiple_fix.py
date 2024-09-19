94800ce4c7e4eb3e67b320aef0995ab686265f11
DavidIraheta <david.iraheta.89@gmail.com>
2024-09-19 14:13:55 -0600

    Add files via upload

---
+++ 07_05_multiple_fix.py
@@ -0,0 +1,8 @@
+# Fix the variable assignment shown below.
+# Can you see why using the multiple variable assignment can be tricky?
+# Declared like this, it's easy to mix which is which.
+
+dreams = "dream_flying"
+profession = "profession_programming"
+print(dreams)
+print(profession)
